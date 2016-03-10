73d7ea57387e7d1c730b03534b61061b9e3fd13d
Camel <fairy_xzc@163.com>
2016-03-10 22:10:02 +0800

    initial commit

---
+++ src/main/java/org/fool/cassandra/CassandraDemo.java
@@ -0,0 +1,20 @@
+package org.fool.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Metadata;
+
+public class CassandraDemo {
+	public static void main(String[] args) {
+		Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+
+		Metadata metadata = cluster.getMetadata();
+		
+		metadata.getAllHosts().forEach(host -> System.out.println(host.getAddress()));
+
+		System.out.println("===============");
+
+		metadata.getKeyspaces().forEach(keyspaceMetadata -> System.out.println(keyspaceMetadata.getName()));
+		
+		cluster.close();
+	}
+}
